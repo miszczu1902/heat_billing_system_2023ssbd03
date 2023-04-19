02363a0524c70e8fe131875f921fea5261b0b840
236574 Krolikowski Hubert <236574@edu.p.lodz.pl>
2023-04-19 17:56:23 +0200

    SSBD202303-78 Poprawa uprawnien do sekwencji i tabel

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,26 +1,5 @@
 CREATE OR REPLACE VIEW public.glassfish_auth_view AS SELECT username, password, access_level FROM account JOIN access_level_mapping ON account.id = access_level_mapping.account_id WHERE account.is_active = true AND account.is_enable = true;
 
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE access_level_mapping TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE account TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE owner TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE admin TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE manager TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE personal_data TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE login_data TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE building TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE address TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE annual_balance TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE heat_distribution_centre TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE heat_distribution_centre_pay_off TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE hot_water_entry TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE month_pay_off TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE past_quarter_hot_water_pay_off TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE place TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE advance TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE hot_water_advance TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE heating_place_and_communal_area_advance TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE glassfish_auth_view TO ssbd03admin;
-
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE glassfish_auth_view TO ssbd03auth;
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE access_level_mapping TO ssbd03mok;
@@ -51,19 +30,6 @@ GRANT SELECT,INSERT,UPDATE ON TABLE advance TO ssbd03mow;
 GRANT SELECT,INSERT,UPDATE ON TABLE hot_water_advance TO ssbd03mow;
 GRANT SELECT,INSERT,UPDATE ON TABLE heating_place_and_communal_area_advance TO ssbd03mow;
 
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE access_level_mapping_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE account_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE address_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE advance_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE annual_balance_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE building_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE heat_distribution_centre_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE heat_distribution_centre_pay_off_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE hot_water_entry_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE month_pay_off_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE past_quarter_hot_water_pay_off_id_seq TO ssbd03admin;
-GRANT SELECT,INSERT,DELETE,UPDATE ON SEQUENCE place_id_seq TO ssbd03admin;
-
 GRANT SELECT,UPDATE ON SEQUENCE access_level_mapping_id_seq TO ssbd03mok;
 GRANT SELECT,UPDATE ON SEQUENCE account_id_seq TO ssbd03mok;
 
@@ -75,5 +41,4 @@ GRANT SELECT,UPDATE ON SEQUENCE heat_distribution_centre_id_seq TO ssbd03mow;
 GRANT SELECT,UPDATE ON SEQUENCE heat_distribution_centre_pay_off_id_seq TO ssbd03mow;
 GRANT SELECT,UPDATE ON SEQUENCE hot_water_entry_id_seq TO ssbd03mow;
 GRANT SELECT,UPDATE ON SEQUENCE month_pay_off_id_seq TO ssbd03mow;
-GRANT SELECT,UPDATE ON SEQUENCE past_quarter_hot_water_pay_off_id_seq TO ssbd03mow;
 GRANT SELECT,UPDATE ON SEQUENCE place_id_seq TO ssbd03mow;
