e77ac16b2ea775e759d11214d63ff04fda2f262b
236576 Kucharski Jakub <236576@edu.p.lodz.pl>
2023-04-28 15:00:32 +0200

    SSBD202303-92 - Wyświetlanie danych o swoim koncie

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/util/converters/AccountConverter.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/util/converters/AccountConverter.java
@@ -1,9 +1,9 @@
 package pl.lodz.p.it.ssbd2023.ssbd03.util.converters;
 
-import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.AdminDTO;
-import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.ManagerDTO;
-import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.OwnerDTO;
-import pl.lodz.p.it.ssbd2023.ssbd03.entities.*;
+import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.CreateOwnerDTO;
+import pl.lodz.p.it.ssbd2023.ssbd03.entities.Account;
+import pl.lodz.p.it.ssbd2023.ssbd03.entities.Owner;
+import pl.lodz.p.it.ssbd2023.ssbd03.entities.PersonalData;
 
 public class AccountConverter {
 
