cd23b6148a1c851f0521c920d282f79357a81f06
236576 Kucharski Jakub <236576@edu.p.lodz.pl>
2023-04-28 15:00:32 +0200

    SSBD202303-92 - Wyświetlanie danych o swoim koncie

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/util/converters/AccountConverter.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/util/converters/AccountConverter.java
@@ -1,6 +1,7 @@
 package pl.lodz.p.it.ssbd2023.ssbd03.util.converters;
 
 import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.CreateOwnerDTO;
+import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.OwnerDTO;
 import pl.lodz.p.it.ssbd2023.ssbd03.entities.Account;
 import pl.lodz.p.it.ssbd2023.ssbd03.entities.Owner;
 import pl.lodz.p.it.ssbd2023.ssbd03.entities.PersonalData;
@@ -18,4 +19,9 @@ public class AccountConverter {
     public static AdminDTO createAdminDTOEntity(Admin admin, PersonalData personalData) {
         return new AdminDTO(admin.getAccount().getEmail(), admin.getAccount().getUsername(), personalData.getFirstName(), personalData.getSurname(), admin.getAccount().getLanguage_());
     }
+
+    public static OwnerDTO createOwnerDTOEntity(Owner owner, PersonalData personalData) {
+        return new OwnerDTO(owner.getId(), owner.getVersion(), owner.getAccount().getEmail(), owner.getAccount().getUsername(), personalData.getFirstName(), personalData.getSurname(), owner.getAccount().getLanguage_(), owner.getPhoneNumber());
+    }
+
 }
