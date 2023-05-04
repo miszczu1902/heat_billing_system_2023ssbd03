ba64778315f527059f3d8b5e85f00d878f25f591
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2023-05-04 23:06:36 +0200

    SSBD202303-122 MOK 9 poprawki MR

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/mok/cdi/endpoints/AccountEndpoint.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/mok/cdi/endpoints/AccountEndpoint.java
@@ -97,7 +97,7 @@ public class AccountEndpoint {
     @RolesAllowed({Roles.ADMIN, Roles.OWNER, Roles.MANAGER})
     public Response editPersonalData(@NotNull @Valid PersonalDataDTO personalDataDTO){
         try {
-        accountService.editPersonalData(personalDataDTO.getFirstName(), personalDataDTO.getSurname());
+        accountService.editSelfPersonalData(personalDataDTO.getFirstName(), personalDataDTO.getSurname());
         return Response.status(Response.Status.OK).build();
         }
         catch (NoResultException e) {
