665727b89e6bd114ee17aac19bbd499a6ffd88c6
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2023-05-05 13:42:24 +0200

    SSBD202303-119 MOK. 7 należy zaimplementować front dla MOK. 7

---
+++ front/ssbd03front/src/components/personalData/EditPersonalData.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import NavbarPanel from '../navigation/NavbarPanel';
+import { TextField } from '@mui/material';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+export default function DialogSelect() {
+  const [open, setOpen] = React.useState(false);
+  var [name, setName] = React.useState('');
+  var [surname, setSurname] = React.useState('');
+
+  const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+}
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    name = event.target.value;
+  };
+
+  const handleSurnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    surname = event.target.value;   
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+    if (reason !== 'backdropClick') {
+      setOpen(false);
+    }
+  };
+
+  return (
+    <div>
+      <NavbarPanel/>
+      <div>
+      <Button onClick={handleClickOpen} variant="contained">Edytuj dane</Button>
+      </div>
+      <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
+        <DialogTitle>Wypełnij formularz edycji danych osobowych</DialogTitle>
+        <DialogContent>
+          <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <form onSubmit={handleSumbit}>
+              <List component="nav" aria-label="mailbox folders">
+                <ListItem>
+                  <div className="form-group" onChange={handleNameChange}>
+                    <TextField
+                      id="outlined-helperText"
+                      label="Imię"
+                      defaultValue="Default Value"
+                      helperText="Wprowadź imię o maksymalniej ilości znaków 32"
+                    />
+                  </div>
+                </ListItem>
+                <ListItem>
+                  <div className="form-group" onChange={handleSurnameChange}>
+                    <TextField
+                      id="outlined-helperText"
+                      label="Nazwisko"
+                      defaultValue="Default Value"
+                      helperText="Wprowadź nazwisko o maksymalniej ilości znaków 32"
+                    />
+                  </div>
+                </ListItem>
+              </List>
+            </form>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
