acaae9b09a35320165c5d6985a1093771f5593ea
236576 Kucharski Jakub <236576@edu.p.lodz.pl>
2023-05-06 20:11:42 +0200

    SSBD202303-92 - Wyświetlanie danych o Manager, Admin

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/dto/request/ManagerDTO.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/dto/request/ManagerDTO.java
@@ -1,13 +1,9 @@
 package pl.lodz.p.it.ssbd2023.ssbd03.dto.request;
 
 public class ManagerDTO extends AccountDTO {
-
     public String firstName;
-
     public String surname;
-
     public String language;
-
     public String license;
 
     public ManagerDTO(String email, String username, String firstName, String surname, String language, String license) {
