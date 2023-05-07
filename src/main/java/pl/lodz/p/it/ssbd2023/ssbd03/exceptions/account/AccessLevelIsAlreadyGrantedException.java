48ebf02dfc264111c27813d521b8208fd2b51b77
Radek <236607@edu.p.lodz.pl>
2023-05-07 22:32:28 +0200

    SSBD202303-127 poprawa v2

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/exceptions/account/AccessLevelIsAlreadyGrantedException.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/exceptions/account/AccessLevelIsAlreadyGrantedException.java
@@ -5,8 +5,8 @@ import jakarta.ws.rs.core.Response;
 import pl.lodz.p.it.ssbd2023.ssbd03.exceptions.AppException;
 
 @ApplicationException(rollback = true)
-public class AccountIsNotActivated extends AppException {
-    public AccountIsNotActivated(String message, Response.Status status) {
+public class AccessLevelIsAlreadyGrantedException extends AppException {
+    public AccessLevelIsAlreadyGrantedException(String message, Response.Status status) {
         super(message, status);
     }
 }
