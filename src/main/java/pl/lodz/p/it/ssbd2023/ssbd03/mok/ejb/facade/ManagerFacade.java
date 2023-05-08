d3182c7a116983b0c9eab6571b6588cfc77afde0
236576 Kucharski Jakub <236576@edu.p.lodz.pl>
2023-05-08 18:09:52 +0200

    SSBD202303-92 - Fixed unused imports

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/mok/ejb/facade/ManagerFacade.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/mok/ejb/facade/ManagerFacade.java
@@ -5,13 +5,16 @@ import jakarta.ejb.TransactionAttribute;
 import jakarta.ejb.TransactionAttributeType;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
+import jakarta.interceptor.Interceptors;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import pl.lodz.p.it.ssbd2023.ssbd03.common.AbstractFacade;
 import pl.lodz.p.it.ssbd2023.ssbd03.entities.Manager;
+import pl.lodz.p.it.ssbd2023.ssbd03.interceptors.TrackerInterceptor;
 
 @Stateless
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
+@Interceptors({TrackerInterceptor.class})
 public class ManagerFacade extends AbstractFacade<Manager> {
     @PersistenceContext(unitName = "ssbd03mokPU")
     private EntityManager em;
