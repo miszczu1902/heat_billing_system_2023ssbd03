c92e009e01ba458678afe0ea9f01ba5959482a89
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2023-05-08 19:31:25 +0200

    SSBD202303-114 implemented usecase

---
+++ front/ssbd03front/src/components/navigation/NavbarPanel.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { ButtonGroup } from '@mui/material';
+
+const NavbarPanel: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+  const [language, setLanguage] = React.useState<string>('');
+
+  const handleChange = (event: SelectChangeEvent<typeof language>) => {
+    setLanguage(event.target.value);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+    if (reason !== 'backdropClick') {
+      setOpen(false);
+    }
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Rozliczenie ciepła dla lokali w wielu budynkach
+        </Typography>
+        <ButtonGroup variant="contained" aria-label="outlined primary button group">
+          <Button>Zaloguj</Button>
+          <Button>Zarejestruj</Button>
+          <Button onClick={handleClickOpen}>Zmień język</Button>
+        </ButtonGroup>
+      </Toolbar>
+      <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
+        <DialogTitle>Zmień język</DialogTitle>
+        <DialogContent>
+          <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="demo-dialog-select-label">Język</InputLabel>
+              <Select
+                labelId="demo-dialog-select-label"
+                id="demo-dialog-select"
+                value={language}
+                onChange={handleChange}
+                input={<OutlinedInput label="Język" />}
+              >
+                <MenuItem value={'PL'}>Polski</MenuItem>
+                <MenuItem value={'EN'}>Angielski</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </AppBar>
+  );
+};
+
+export default NavbarPanel;
