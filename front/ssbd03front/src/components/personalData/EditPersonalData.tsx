e90cb004a14f9336a759b0f7e5a1c6b27fc57585
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2023-05-11 22:32:22 +0200

    SSBD202303-119 full implementation

--- front/ssbd03front/src/components/personalData/EditPersonalData.tsx
+++ front/ssbd03front/src/components/personalData/EditPersonalData.tsx
@@ -5,29 +5,37 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import NavbarPanel from '../navigation/NavbarPanel';
 import { TextField } from '@mui/material';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import axios from 'axios'; 
 import validator from "validator";
+import { API_URL } from '../../consts';
 import { useState, useEffect } from 'react';
+import e from 'express';
 
-const GET_DATA_URL = 'http://localhost:8080/api/accounts/self/personal-data';
 const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtaXN6Y3p1MjEzNyIsImlhdCI6MTY4MzU2NTAxNywicm9sZSI6Ik9XTkVSIiwiZXhwIjoxNjgzNTY2ODE3fQ.ddfkn394c0xhJsY2d58P5uQGJD7Lu4iY3Of7stt9NgI';
 
 export default function EditPersonalData() {
   const [open, setOpen] = React.useState(false);
   const [confirmOpen, setConfirmOpen] = React.useState(false);
+
   var [name, setName] = React.useState("");
   var [surname, setSurname] = React.useState("");
   var [newName, setNewName] = React.useState("");
   var [newSurname, setNewSurname] = React.useState("");
+
   var [nameError, setNameError] = React.useState("");
   var [surnameError, setSurnameError] = React.useState("");
+  var [dataError, setDataError] = React.useState("");
+
+  var [validData, setValidData] = React.useState(false);
+
+  var [successOpen, setSuccessOpen] = React.useState(false);
+  var [errorOpen, setErrorOpen] = React.useState(false);
 
   useEffect(() => {
-    axios.get(GET_DATA_URL, {
+    axios.get(`${API_URL}/accounts/self/personal-data`, {
       headers: {
         Authorization: 'Bearer ' + token
       }
@@ -45,25 +53,29 @@ export default function EditPersonalData() {
     e.preventDefault();
 }
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (validator.isAlpha(event.target.value) && event.target.value.length <= 32) {
+    if (validator.isAlpha(event.target.value) && event.target.value.length <= 32 && event.target.value.length > 0) {
       setNameError("");
       newName = event.target.value;
+      setValidData(true);
     } else {
       setNameError("Imię może zawierać tylko litery i musi mieć długość do 32 znaków");
+      setValidData(false);
     }
   };
 
   const handleSurnameChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
-    if (validator.isAlpha(event.target.value) && event.target.value.length <= 32) {
+    if (validator.isAlpha(event.target.value) && event.target.value.length <= 32 && event.target.value.length > 0) {
       setSurnameError("");
       newSurname = event.target.value;
+      setValidData(true);
     } else {
       setSurnameError("Nazwisko może zawierać tylko litery i musi mieć długość do 32 znaków");
+      setValidData(false); 
     } 
   };
 
   const handleClickOpen = () => {
-    axios.get(GET_DATA_URL, {
+    axios.get(`${API_URL}/accounts/self/personal-data`, {
       headers: {
         Authorization: 'Bearer ' + token
       }
@@ -98,28 +110,47 @@ export default function EditPersonalData() {
     setSurname(newSurname);
     if(nameError === "" && surnameError === "") {
       const data = { firstName: name, surname: surname};
-      axios.patch(GET_DATA_URL, {
+      axios.patch(`${API_URL}/accounts/self/personal-data`, {
         headers: {
           Authorization: 'Bearer ' + token
         }, 
         data: data
       })
       .then(response => {
+        setSuccessOpen(true);
       })
       .catch(error => {
-        console.log(error);
+        setErrorOpen(true);
       });
     }
     handleClose(event, reason);
   }
 
   const handleConfirm = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-    setConfirmOpen(true);
-  };
+    if(validData) {
+      setDataError("");
+      setConfirmOpen(true);
+    } else {
+      setDataError("Wprowadź poprawne dane");
+    }
+  }
+  
+  const handleSuccessClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+    if (reason !== 'backdropClick') {
+      setSuccessOpen(false);
+    }
+  }
+
+  const handleErrorClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+    if (reason !== 'backdropClick') {
+      setErrorOpen(false);
+    }
+  }
+  
+  ;
 
   return (
     <div>
-      <NavbarPanel/>
       <div>
       <Button onClick={handleClickOpen} variant="contained">Edytuj dane</Button>
       </div>
@@ -156,6 +187,9 @@ export default function EditPersonalData() {
                   </div>
                 </ListItem>
               </List>
+              <div className="form-group">
+                      {dataError}
+              </div>
             </form>
           </Box>
         </DialogContent>
@@ -172,6 +206,14 @@ export default function EditPersonalData() {
           <Button onClick={handleConfirmClose}>Nie</Button>
           <Button onClick={handleConfirmConfirm}>Tak</Button>
         </DialogActions>
+      </Dialog>
+
+      <Dialog disableEscapeKeyDown open={successOpen} onClose={handleSuccessClose}>
+        <DialogTitle>Dane osobowe zostały zmienione</DialogTitle>
+      </Dialog> 
+
+      <Dialog disableEscapeKeyDown open={errorOpen} onClose={handleErrorClose}>
+        <DialogTitle>Wystąpił błąd podczas zmiany danych osobowych</DialogTitle>
       </Dialog>
     </div>
   );
