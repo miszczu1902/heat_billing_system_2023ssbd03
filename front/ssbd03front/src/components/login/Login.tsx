2dc19567520704fc8588e01df55d292c33c99556
236710 Zuchora Bartosz <236710@edu.p.lodz.pl>
2023-05-12 22:10:01 +0200

    SSBD202303-178 bazowy front bez CORS

---
+++ front/ssbd03front/src/components/login/Login.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import {API_URL} from '../../consts';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import axios from 'axios';
+import {useCookies} from 'react-cookie';
+
+const theme = createTheme();
+
+export default function Login() {
+    const [cookies, setCookie] = useCookies(["token"]);
+    var [username, setUsername] = React.useState("");
+    var [password, setPassword] = React.useState("");
+    var [loginError, setLoginError] = React.useState("");
+
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (username.length < 6) {
+            setLoginError('Login musi mieć przynajmniej 6 znaków');
+        } else if (password.length < 8) {
+            setPassword("");
+            setLoginError('Hasło musi mieć przynajmniej 8 znaków');
+        } else if (username.length >= 16) {
+
+            setLoginError('Login może mieć maksymalnie 16 znaków');
+        } else {
+            let data = JSON.stringify({
+                "username": username,
+                "password": password
+                // "username": "johndoe",
+                // "password": "Password$123"
+            });
+
+            let config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: API_URL + '/accounts/login',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: data,
+            };
+            axios.request(config)
+                .then((response) => {
+                    setCookie("token", response.data)
+                    window.location.href = `/accounts`;
+                })
+                .catch((error) => {
+                    setPassword("");
+                    setLoginError(error.response.data.message);
+                });
+        }
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid container justifyContent="center" alignItems="center">
+                <Grid my={2} item sm={8} md={5} component={Paper} elevation={6}>
+                    <Box sx={{my: 30, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <Typography variant="h5"> Logowanie </Typography>
+                        <p>{loginError}</p>
+                        <Box component="form" onSubmit={handleSubmit}>
+                            <Box component="form">
+                                <TextField fullWidth margin="normal" label="Login" value={username}
+                                           helperText="Wprowadź Login" onChange={handleUsernameChange}/>
+                                <TextField fullWidth margin="normal" label="Hasło" type="password"
+                                           helperText="Wprowadź hasło" onChange={handlePasswordChange}
+                                           value={password}/>
+                            </Box>
+                            <Button type="submit" fullWidth variant="contained">Zaloguj</Button>
+                            <Box sx={{my: 1, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                                <Link href="????">Zapomniałeś hasła?</Link>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
+}
