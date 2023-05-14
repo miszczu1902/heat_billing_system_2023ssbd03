61fb16a0176682db5c7e386b265582e3fad81fe3
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2023-05-14 15:45:24 +0200

    SSBD202303-177 Działający ETag dla operacji enable i disable na koncie (self + innego usera)

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/entities/Account.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/entities/Account.java
@@ -108,9 +108,9 @@ public class Account extends AbstractEntity implements Signable {
                 .concat(getUsername())
                 .concat(getIsEnable().toString())
                 .concat(getIsActive().toString())
-                .concat(getRegisterDate().toString())
-                .concat(getAccessLevels().stream().toString())
                 .concat(getPersonalData().getFirstName())
-                .concat(getPersonalData().getSurname());
+                .concat(getPersonalData().getSurname())
+                .concat(getId().toString())
+                .concat(getVersion().toString());
     }
 }
