3efe44b29459606bf2e27f54e3238a53d86a9c87
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-05-15 11:27:05 +0200

    SSBD202303-143 - landing page dodany

--- front/ssbd03front/src/components/landingPage/LandingPage.tsx
+++ front/ssbd03front/src/components/landingPage/LandingPage.tsx
@@ -1,5 +1,45 @@
+import {Button, Container, Grid, Typography} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import logo from './../../assets/logo.svg';
+
 const LandingPage = () => {
-    return (<div/>);
+    const navigate = useNavigate();
+
+    const handleButtonClick = (path: string) => {
+        navigate(path);
+    }
+
+    return (
+        <div className="landing-page-root">
+            <Container>
+                <img src={logo} alt="Logo"/>
+            </Container>
+            <Container maxWidth="sm">
+                <Grid container direction="column" alignItems="center" spacing={4}>
+                    <Grid item>
+                        <Typography variant="h4" component="h1">
+                            Projekt zespołu 3 SSBD 2023
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="subtitle1">
+                            System rozliczania ciepła w lokalach w wielu budynkach.
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button className="landing-page-button" variant="contained" color="primary"
+                                onClick={() => handleButtonClick('/register')}>
+                            Zarejestruj się
+                        </Button>
+                        <Button className="landing-page-button" variant="contained" color="primary"
+                                onClick={() => handleButtonClick('/login')}>
+                            Zaloguj się
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
 
 export default LandingPage;
