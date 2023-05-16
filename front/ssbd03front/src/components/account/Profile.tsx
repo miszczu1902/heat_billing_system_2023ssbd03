905983307f20dd382ee24f45ac2109344176c56d
Radek <236607@edu.p.lodz.pl>
2023-05-16 14:29:29 +0200

    SSBD202303-175 usunięcie duplikatów

--- front/ssbd03front/src/components/account/Profile.tsx
+++ front/ssbd03front/src/components/account/Profile.tsx
@@ -27,33 +27,153 @@ export default function Profile() {
     const [phoneNumber, setPhoneNumber] = useState("");
     const username = useParams().username;
 
-    const [confirmOpenAdmin, setConfirmOpenAdmin] = React.useState(false);
-    const [successOpenAdmin, setSuccessOpenAdmin] = React.useState(false);
-    const [errorOpenAdmin, setErrorOpenAdmin] = React.useState(false);
-    const [errorOpenMessageAdmin, setErrorOpenMessageAdmin] = React.useState("");
-    const [openAdmin, setOpenAdmin] = React.useState(false);
-
-    const [confirmOpenOwner, setConfirmOpenOwner] = React.useState(false);
-    const [successOpenOwner, setSuccessOpenOwner] = React.useState(false);
-    const [errorOpenOwner, setErrorOpenOwner] = React.useState(false);
-    const [errorOpenMessageOwner, setErrorOpenMessageOwner] = React.useState("");
-    const [openOwner, setOpenOwner] = React.useState(false);
     const [phoneNumberError, setPhoneNumberError] = React.useState("");
-    const [phoneNumberValid, setPhoneNumberValid] = React.useState(false);
-
-    const [confirmOpenManager, setConfirmOpenManager] = React.useState(false);
-    const [successOpenManager, setSuccessOpenManager] = React.useState(false);
-    const [errorOpenManager, setErrorOpenManager] = React.useState(false);
-    const [errorOpenMessageManager, setErrorOpenMessageManager] = React.useState("");
-    const [openManager, setOpenManager] = React.useState(false);
     const [licenseError, setLicenseError] = React.useState("");
+    const [phoneNumberValid, setPhoneNumberValid] = React.useState(false);
     const [licenseValid, setLicenseValid] = React.useState(false);
 
+    const [confirmOpen, setConfirmOpen] = React.useState(false);
+    const [successOpen, setSuccessOpen] = React.useState(false);
+
+    const [errorOpen, setErrorOpen] = React.useState(false);
+    const [errorOpenMessage, setErrorOpenMessage] = React.useState("");
     const [dataError, setDataError] = React.useState("");
 
+    const [isAdmin, setIsAdmin] = React.useState(false);
+    const [isOwner, setIsOwner] = React.useState(false);
+    const [isManager, setIsManager] = React.useState(false);
 
+    const handleConfirmConfirm = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+        if (reason !== 'backdropClick') {
+            setConfirmOpen(false);
+        }
+        if (isManager && !isOwner && !isAdmin) {
+            const addAccessLevelManagerDTO = {
+                username: username,
+                license: license.toString()
+            }
 
-    ///////////////////////
+            axios.patch(`${API_URL}/accounts/add-access-level-manager`,
+                addAccessLevelManagerDTO, {
+                    headers: {
+                        'Authorization': token,
+                        'Content-Type': 'application/json'
+                    },
+                })
+                .then(response => {
+                    setSuccessOpen(true);
+                })
+                .catch(error => {
+                    setErrorOpenMessage(error.response.data.message)
+                    setErrorOpen(true);
+                });
+            handleClose(event, reason);
+        }
+        if (isOwner && !isManager && !isAdmin) {
+            const addAccessLevelOwnerDTO = {
+                username: username,
+                phoneNumber: phoneNumber.toString()
+            }
+
+            axios.patch(`${API_URL}/accounts/add-access-level-owner`,
+                addAccessLevelOwnerDTO, {
+                    headers: {
+                        'Authorization': token,
+                        'Content-Type': 'application/json'
+                    },
+                })
+                .then(response => {
+                    setSuccessOpen(true);
+                })
+                .catch(error => {
+                    setErrorOpenMessage(error.response.data.message)
+                    setErrorOpen(true);
+                });
+            handleClose(event, reason);
+        }
+        if (isAdmin && !isOwner && !isManager) {
+            const addAccessLevelAdminDTO = {
+                username: username
+            }
+
+            axios.patch(`${API_URL}/accounts/add-access-level-admin`,
+                addAccessLevelAdminDTO, {
+                    headers: {
+                        'Authorization': token,
+                        'Content-Type': 'application/json'
+                    },
+                })
+                .then(response => {
+                    setSuccessOpen(true);
+                })
+                .catch(error => {
+                    setErrorOpenMessage(error.response.data.message)
+                    setErrorOpen(true);
+                });
+            handleClose(event, reason);
+        }
+    }
+
+    const handleClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+        if (reason !== 'backdropClick') {
+            setIsManager(false);
+            setIsOwner(false);
+            setIsAdmin(false);
+        }
+    };
+
+    const handleErrorClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+        if (reason !== 'backdropClick') {
+            setErrorOpen(false);
+            setConfirmOpen(false);
+            setIsManager(false);
+            setIsOwner(false);
+            setIsAdmin(false);
+        }
+    };
+
+    const handleSuccessClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+        setLicense("");
+        setPhoneNumber("");
+        setIsManager(false);
+        setIsOwner(false);
+        setIsAdmin(false);
+        if (reason !== 'backdropClick') {
+            setSuccessOpen(false);
+        }
+    }
+
+    const handleConfirm = () => {
+        if (licenseValid || phoneNumberValid) {
+            setDataError("");
+            setConfirmOpen(true);
+        } else {
+            setDataError("Wprowadź poprawne dane");
+        }
+    }
+
+    const handleSumbit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+    const handleConfirmClose = (event: React.SyntheticEvent<unknown>, reason?: string) => {
+        if (reason !== 'backdropClick') {
+            setConfirmOpen(false);
+        }
+    }
+
+    const handlePhoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let phoneNumber = event.target.value;
+        setPhoneNumber(phoneNumber);
+        const regex = /^\d{9}$/;
+        if (!regex.test(phoneNumber)) {
+            setPhoneNumberError("Numer telefonu musi zawierać 9 cyfr.");
+            setPhoneNumberValid(false);
+        } else {
+            setPhoneNumberError("");
+            setPhoneNumberValid(true);
+        }
+    };
 
     const handleLicense = (event: React.ChangeEvent<HTMLInputElement>) => {
         let license = event.target.value;
@@ -68,206 +188,18 @@ export default function Profile() {
         }
     };
 
-    const handleConfirmConfirmManager = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setConfirmOpenManager(false);
-        }
-        const addAccessLevelManagerDTO = {
-            username: username,
-            license: license.toString()
-        }
-
-        axios.patch(`${API_URL}/accounts/add-access-level-manager`,
-            addAccessLevelManagerDTO, {
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json'
-                },
-            })
-            .then(response => {
-                setSuccessOpenManager(true);
-            })
-            .catch(error => {
-                setErrorOpenMessageManager(error.response.data.message)
-                setErrorOpenManager(true);
-            });
-        handleCloseManager(event, reason);
-    }
-
-    const handleCloseManager = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setOpenManager(false);
-        }
-    };
-
-    const handleErrorCloseManager = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setErrorOpenManager(false);
-            setConfirmOpenManager(false);
-        }
-    };
-
     const handleClickOpenManager = () => {
-        setOpenManager(true);
-    };
-
-    const handleSuccessCloseManager = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        setLicense("");
-        if (reason !== 'backdropClick') {
-            setSuccessOpenManager(false);
-        }
-    }
-
-    const handleConfirmManager = () => {
-        if (licenseValid) {
-            setDataError("");
-            setConfirmOpenManager(true);
-        } else {
-            setDataError("Wprowadź poprawne dane");
-        }
-    }
-
-    const handleSumbitManager = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    }
-
-    const handleConfirmCloseManager = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setConfirmOpenManager(false);
-        }
-    }
-
-//////////////////
-    const handlePhoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
-        let phoneNumber = event.target.value;
-        setPhoneNumber(phoneNumber);
-        const regex = /^\d{9}$/;
-        if (!regex.test(phoneNumber)) {
-            setPhoneNumberError("Numer telefonu musi zawierać 9 cyfr.");
-            setPhoneNumberValid(false);
-        } else {
-            setPhoneNumberError("");
-            setPhoneNumberValid(true);
-        }
-    };
-
-    const handleConfirmConfirmOwner = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setConfirmOpenAdmin(false);
-        }
-        const addAccessLevelOwnerDTO = {
-            username: username,
-            phoneNumber: phoneNumber.toString()
-        }
-
-        axios.patch(`${API_URL}/accounts/add-access-level-owner`,
-            addAccessLevelOwnerDTO, {
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json'
-                },
-            })
-            .then(response => {
-                setSuccessOpenAdmin(true);
-            })
-            .catch(error => {
-                setErrorOpenMessageAdmin(error.response.data.message)
-                setErrorOpenAdmin(true);
-            });
-        handleCloseOwner(event, reason);
-    }
-
-    const handleCloseOwner = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setOpenOwner(false);
-        }
-    };
-
-    const handleErrorCloseOwner = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setErrorOpenOwner(false);
-            setConfirmOpenOwner(false);
-        }
+        setIsManager(true);
     };
 
     const handleClickOpenOwner = () => {
-        setOpenOwner(true);
+        setIsOwner(true);
     };
-
-    const handleSuccessCloseOwner = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        setPhoneNumber("");
-        if (reason !== 'backdropClick') {
-            setSuccessOpenOwner(false);
-        }
-    }
-
-    const handleConfirmOwner = () => {
-        if (phoneNumberValid) {
-            setDataError("");
-            setConfirmOpenOwner(true);
-        } else {
-            setDataError("Wprowadź poprawne dane");
-        }
-    }
-
-    const handleSumbitOwner = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    }
-
-    const handleConfirmCloseOwner = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setConfirmOpenOwner(false);
-        }
-    }
-
-    /////////////
-
-    const handleConfirmConfirmAdmin = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setConfirmOpenAdmin(false);
-        }
-        const addAccessLevelAdminDTO = {
-            username: username
-        }
-
-        axios.patch(`${API_URL}/accounts/add-access-level-admin`,
-            addAccessLevelAdminDTO, {
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json'
-                },
-            })
-            .then(response => {
-                setSuccessOpenAdmin(true);
-            })
-            .catch(error => {
-                setErrorOpenMessageAdmin(error.response.data.message)
-                setErrorOpenAdmin(true);
-            });
-        handleCloseAdmin(event, reason);
-    }
 
     const handleClickOpenAdmin = () => {
-        setOpenAdmin(true);
+        setIsAdmin(true);
     };
 
-    const handleCloseAdmin = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setOpenAdmin(false);
-        }
-    };
-    const handleErrorCloseAdmin = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        if (reason !== 'backdropClick') {
-            setErrorOpenAdmin(false);
-        }
-    };
-
-    const handleSuccessCloseAdmin = (event: React.SyntheticEvent<unknown>, reason?: string) => {
-        setPhoneNumber("");
-        if (reason !== 'backdropClick') {
-            setSuccessOpenAdmin(false);
-        }
-    }
     return (
         <div>
             <Button onClick={handleClickOpenManager} variant="contained">
@@ -280,11 +212,11 @@ export default function Profile() {
                 Dodaj poziom dostępu admin
             </Button>
 
-            <Dialog disableEscapeKeyDown open={openManager} onClose={handleCloseManager}>
+            <Dialog disableEscapeKeyDown open={isManager} onClose={handleClose}>
                 <DialogTitle>Wypełnij formularz dodania uprawnien zarządcy</DialogTitle>
                 <DialogContent>
                     <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
-                        <form onSubmit={handleSumbitManager}>
+                        <form onSubmit={handleSumbit}>
                             <List component="nav" aria-label="mailbox folders">
                                 <ListItem>
                                     <div className="form-group" onChange={handleLicense}>
@@ -308,34 +240,24 @@ export default function Profile() {
                     </Box>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleCloseManager}>Cancel</Button>
-                    <Button onClick={handleConfirmManager} disabled={!license}>Ok</Button>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleConfirm} disabled={!license}>Ok</Button>
                 </DialogActions>
             </Dialog>
 
-            <Dialog disableEscapeKeyDown open={confirmOpenManager} onClose={handleConfirmCloseManager}>
+            <Dialog disableEscapeKeyDown open={confirmOpen} onClose={handleConfirmClose}>
                 <DialogTitle>Czy na pewno chcesz dodać poziom dostępu zarządcy?</DialogTitle>
                 <DialogActions>
-                    <Button onClick={handleConfirmCloseManager}>Nie</Button>
-                    <Button onClick={handleConfirmConfirmManager}>Tak</Button>
+                    <Button onClick={handleConfirmClose}>Nie</Button>
+                    <Button onClick={handleConfirmConfirm}>Tak</Button>
                 </DialogActions>
             </Dialog>
 
-            <Dialog disableEscapeKeyDown open={successOpenManager}>
-                <DialogTitle>Poziom dostępu zarzadcy został dodany</DialogTitle>
-                <Button onClick={handleSuccessCloseManager}>Ok</Button>
-            </Dialog>
-
-            <Dialog disableEscapeKeyDown open={errorOpenManager}>
-                <DialogTitle>{errorOpenMessageManager}</DialogTitle>
-                <Button onClick={handleErrorCloseManager}>Ok</Button>
-            </Dialog>
-
-            <Dialog disableEscapeKeyDown open={openOwner} onClose={handleCloseAdmin}>
+            <Dialog disableEscapeKeyDown open={isOwner} onClose={handleClose}>
                 <DialogTitle>Wypełnij formularz dodania uprawnieć właściciela</DialogTitle>
                 <DialogContent>
                     <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
-                        <form onSubmit={handleSumbitOwner}>
+                        <form onSubmit={handleSumbit}>
                             <List component="nav" aria-label="mailbox folders">
                                 <ListItem>
                                     <div className="form-group" onChange={handlePhoneNumber}>
@@ -359,47 +281,36 @@ export default function Profile() {
                     </Box>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleCloseOwner}>Cancel</Button>
-                    <Button onClick={handleConfirmOwner} disabled={!phoneNumberValid}>Ok</Button>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleConfirm} disabled={!phoneNumberValid}>Ok</Button>
                 </DialogActions>
             </Dialog>
 
-            <Dialog disableEscapeKeyDown open={confirmOpenOwner} onClose={handleConfirmCloseOwner}>
+            <Dialog disableEscapeKeyDown open={confirmOpen} onClose={handleConfirmClose}>
                 <DialogTitle>Czy na pewno chcesz dodać poziom dostępu właściciela?</DialogTitle>
                 <DialogActions>
-                    <Button onClick={handleConfirmCloseOwner}>Nie</Button>
-                    <Button onClick={handleConfirmConfirmOwner}>Tak</Button>
+                    <Button onClick={handleConfirmClose}>Nie</Button>
+                    <Button onClick={handleConfirmConfirm}>Tak</Button>
                 </DialogActions>
             </Dialog>
 
-            <Dialog disableEscapeKeyDown open={successOpenOwner}>
-                <DialogTitle>Poziom dostępu właściciela został dodany</DialogTitle>
-                <Button onClick={handleSuccessCloseOwner}>Ok</Button>
-            </Dialog>
-
-            <Dialog disableEscapeKeyDown open={errorOpenOwner}>
-                <DialogTitle>{errorOpenMessageOwner}</DialogTitle>
-                <Button onClick={handleErrorCloseOwner}>Ok</Button>
-            </Dialog>
-
-            <Dialog disableEscapeKeyDown open={openAdmin} onClose={handleCloseAdmin}>
+            <Dialog disableEscapeKeyDown open={isAdmin} onClose={handleClose}>
                 <DialogTitle>Czy na pewno chcesz dodać poziom dostępu admin?</DialogTitle>
                 <DialogActions>
-                    <Button onClick={handleCloseAdmin}>Nie</Button>
-                    <Button onClick={handleConfirmConfirmAdmin}>Tak</Button>
+                    <Button onClick={handleClose}>Nie</Button>
+                    <Button onClick={handleConfirmConfirm}>Tak</Button>
                 </DialogActions>
             </Dialog>
 
-            <Dialog disableEscapeKeyDown open={successOpenAdmin}>
+            <Dialog disableEscapeKeyDown open={successOpen}>
                 <DialogTitle>Poziom dostępu został dodany</DialogTitle>
-                <Button onClick={handleSuccessCloseAdmin}>Ok</Button>
+                <Button onClick={handleSuccessClose}>Ok</Button>
             </Dialog>
 
-            <Dialog disableEscapeKeyDown open={errorOpenAdmin}>
-                <DialogTitle>{errorOpenMessageAdmin}</DialogTitle>
-                <Button onClick={handleErrorCloseAdmin}>Ok</Button>
+            <Dialog disableEscapeKeyDown open={errorOpen}>
+                <DialogTitle>{errorOpenMessage}</DialogTitle>
+                <Button onClick={handleErrorClose}>Ok</Button>
             </Dialog>
         </div>
     );
-
 }
