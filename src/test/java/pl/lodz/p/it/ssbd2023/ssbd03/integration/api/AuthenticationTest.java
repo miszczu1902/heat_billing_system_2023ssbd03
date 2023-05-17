502370f970cc5bef8a18a40359c14e98bce5c123
236710 Zuchora Bartosz <236710@edu.p.lodz.pl>
2023-05-17 22:18:26 +0200

    SSBD202303-208 testy integracyjne MOK 16, 14, 11

--- src/test/java/pl/lodz/p/it/ssbd2023/ssbd03/integration/api/AuthenticationTest.java
+++ src/test/java/pl/lodz/p/it/ssbd2023/ssbd03/integration/api/AuthenticationTest.java
@@ -2,9 +2,12 @@ package pl.lodz.p.it.ssbd2023.ssbd03.integration.api;
 
 import io.restassured.http.ContentType;
 import io.restassured.http.Method;
+import org.junit.Before;
 import org.junit.Test;
+import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
 import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.CreateOwnerDTO;
 import pl.lodz.p.it.ssbd2023.ssbd03.dto.request.LoginDTO;
+import pl.lodz.p.it.ssbd2023.ssbd03.integration.config.Account;
 import pl.lodz.p.it.ssbd2023.ssbd03.integration.config.BasicIntegrationConfigTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,24 +33,60 @@ public class AuthenticationTest extends BasicIntegrationConfigTest {
     @Test
     public void Authenticate() {
         LoginDTO loginDTO = new LoginDTO(
-                "janekowalski", "Password$123"
+                Account.MANAGER, Account.PASSWORD
         );
 
         int statusCode = authenticate(loginDTO);
-        assertEquals(200, statusCode);
+        assertEquals(200, statusCode, "check if status code 200 after authentication");
 
         logger.info("PASSED!");
     }
 
+    @Test
+    public void AuthenticateWithTooShortAndTooLongUsername() {
+        LoginDTO loginDTOShort = new LoginDTO(
+                RandomStringUtils.randomAlphanumeric(5), Account.PASSWORD
+        );
+
+        int statusCode = authenticate(loginDTOShort);
+        assertEquals(400, statusCode, "check if status code is 400 because of too short username");
+
+        LoginDTO loginDTOLong = new LoginDTO(
+                RandomStringUtils.randomAlphanumeric(20), Account.PASSWORD
+        );
+
+        statusCode = authenticate(loginDTOLong);
+        assertEquals(400, statusCode, "check if status code is 400 because of too long username");
+
+        logger.info("PASSED!");
+    }
+
+    @Test
+    public void AuthenticateWithTooShortAndTooLongPassword() {
+        LoginDTO loginDTOShort = new LoginDTO(
+                Account.MANAGER, RandomStringUtils.randomAlphanumeric(5)
+        );
+
+        int statusCode = authenticate(loginDTOShort);
+        assertEquals(400, statusCode, "check if status code is 400 because of too short password");
+
+        LoginDTO loginDTOLong = new LoginDTO(
+                Account.MANAGER, RandomStringUtils.randomAlphanumeric(34)
+        );
+
+        statusCode = authenticate(loginDTOLong);
+        assertEquals(400, statusCode, "check if status code is 400 because of too long password");
+        logger.info("PASSED!");
+    }
 
     @Test
     public void InvalidLogin() {
         LoginDTO loginDTO = new LoginDTO(
-                "mariasilvA", "Password$123"
+                Account.OWNER + "A", Account.PASSWORD
         );
 
         int statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid login");
 
         logger.info("PASSED!");
     }
@@ -55,11 +94,11 @@ public class AuthenticationTest extends BasicIntegrationConfigTest {
     @Test
     public void InvalidPassword() {
         LoginDTO loginDTO = new LoginDTO(
-                "mariasilva", "Password$123!"
+                Account.OWNER, Account.PASSWORD + "!"
         );
 
         int statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password");
 
         logger.info("PASSED!");
     }
@@ -67,20 +106,20 @@ public class AuthenticationTest extends BasicIntegrationConfigTest {
     @Test
     public void InValidCredentialsOnDisabledUser() {
         LoginDTO loginDTO = new LoginDTO(
-                "mariasilva", "Password$123!"
+                Account.OWNER, Account.PASSWORD + "!"
         );
 
         int statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password");
 
         statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password");
 
         statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password");
 
         statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password when account is disabled");
 
         logger.info("PASSED!");
     }
@@ -88,24 +127,24 @@ public class AuthenticationTest extends BasicIntegrationConfigTest {
     @Test
     public void ValidCredentialsOnDisabledUser() {
         LoginDTO loginDTO = new LoginDTO(
-                "johndoe", "Password$123!"
+                Account.ADMIN, Account.PASSWORD + "!"
         );
 
         int statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password");
 
         statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password");
 
         statusCode = authenticate(loginDTO);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after invalid password");
 
         LoginDTO loginDTOValidCredentials = new LoginDTO(
-                "johndoe", "Password$123"
+                Account.ADMIN, Account.PASSWORD
         );
 
         statusCode = authenticate(loginDTOValidCredentials);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 after valid credentials but account is disabled");
 
         logger.info("PASSED!");
     }
@@ -121,6 +160,6 @@ public class AuthenticationTest extends BasicIntegrationConfigTest {
         );
 
         statusCode = authenticate(loginDTOValidCredentials);
-        assertEquals(401, statusCode);
+        assertEquals(401, statusCode, "check if status code 401 when credentials are valid but account is not activated");
     }
 }
