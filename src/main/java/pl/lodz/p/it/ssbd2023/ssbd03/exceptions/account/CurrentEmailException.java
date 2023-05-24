4c1bac99637f32dc335d172f65466d23ab6a174c
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-05-24 12:58:57 +0200

    SSBD2023-274

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/exceptions/account/CurrentEmailException.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/exceptions/account/CurrentEmailException.java
@@ -5,8 +5,8 @@ import jakarta.ws.rs.core.Response;
 import pl.lodz.p.it.ssbd2023.ssbd03.exceptions.AppException;
 
 @ApplicationException(rollback = true)
-public class AccountNotExistsException extends AppException {
-    public AccountNotExistsException(String message, Response.Status status, Throwable cause) {
-        super(message, status, cause);
+public class CurrentEmailException extends AppException {
+    public CurrentEmailException(String message, Response.Status status) {
+        super(message, status);
     }
 }
