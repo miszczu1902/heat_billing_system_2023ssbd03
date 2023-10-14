195a2db0d58b7ce99b03f47e832a7350a6c15ec7
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-10-14 22:20:25 +0200

    Docker compose działa

--- payara/src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/config/Roles.java
+++
@@ -1,8 +0,0 @@
-package pl.lodz.p.it.ssbd2023.ssbd03.config;
-
-public class Roles {
-    public static final String GUEST = "GUEST";
-    public static final String OWNER = "OWNER";
-    public static final String ADMIN = "ADMIN";
-    public static final String MANAGER = "MANAGER";
-}
--- payara/src/test/java/pl/lodz/p/it/ssbd2023/ssbd03/integration/config/Account.java
+++
@@ -1,9 +0,0 @@
-package pl.lodz.p.it.ssbd2023.ssbd03.integration.config;
-
-public class Account {
-    public static final String OWNER = "mariasilva";
-    public static final String OWNER2 = "ownerTestowy";
-    public static final String ADMIN = "johndoe";
-    public static final String MANAGER = "JacksonLee";
-    public static final String PASSWORD = "Password$123";
-}
