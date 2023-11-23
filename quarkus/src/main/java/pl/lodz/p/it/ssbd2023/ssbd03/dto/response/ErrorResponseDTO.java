970333f27a100e8a0f0720cc988ad2d541b2be4f
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-11-23 21:11:11 +0100

    Testowanie s2i

--- quarkus/src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/dto/response/ErrorResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package pl.lodz.p.it.ssbd2023.ssbd03.dto.response;
-
-public record ErrorResponseDTO(Integer statusCode, String message) {
-}
--- quarkus/src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/util/etag/Signable.java
+++
@@ -1,9 +0,0 @@
-package pl.lodz.p.it.ssbd2023.ssbd03.util.etag;
-
-import jakarta.json.bind.annotation.JsonbTransient;
-
-public interface Signable {
-
-    @JsonbTransient
-    String messageToSign();
-}
