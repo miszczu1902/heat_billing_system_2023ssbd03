db79dd6a09611c96eb4bd3ed50151d4fba33c953
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-11-24 19:23:40 +0100

    praca nad health checkami

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/DatabaseMonitor.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/DatabaseMonitor.java
@@ -2,8 +2,10 @@ package pl.lodz.p.it.ssbd2023.ssbd03.health;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Readiness;
 
 import javax.sql.DataSource;
@@ -16,8 +18,23 @@ public class DatabaseMonitor implements HealthCheck {
     @Inject
     DataSource dataSource;
 
+    @Inject
+    @ConfigProperty(name = "memory.threshold", defaultValue = "0.9")
+    double memoryThreshold;
+
     @Override
     public HealthCheckResponse call() {
+        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Polaczenie z API");
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        double memoryUsage = (double) (maxMemory - freeMemory) / maxMemory;
+        if (memoryUsage <= memoryThreshold) {
+            responseBuilder.up()
+                    .withData("usage", "Obecne zuzycie pamieci  %s%%".formatted(memoryUsage * 100));
+        } else {
+            responseBuilder.down().withData("usage", "Obecne zuzycie pamieci %s %%".formatted(memoryUsage * 100));
+        }
+
         try (Connection connection = dataSource.getConnection()) {
             if (connection.isValid(5)) {
                 return HealthCheckResponse.up("Połączenie z bazą danych jest aktywne");
--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/MemoryMonitor.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/MemoryMonitor.java
@@ -1,32 +1,32 @@
-package pl.lodz.p.it.ssbd2023.ssbd03.health;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.health.HealthCheck;
-import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
-import org.eclipse.microprofile.health.Liveness;
-
-@Liveness
-@ApplicationScoped
-public class MemoryMonitor implements HealthCheck {
-    @Inject
-    @ConfigProperty(name = "memory.threshold", defaultValue = "0.9")
-    double memoryThreshold;
-
-    @Override
-    public HealthCheckResponse call() {
-        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Zuzycie pamieci dla SSBD03");
-        long freeMemory = Runtime.getRuntime().freeMemory();
-        long maxMemory = Runtime.getRuntime().maxMemory();
-        double memoryUsage = (double) (maxMemory - freeMemory) / maxMemory;
-        if (memoryUsage <= memoryThreshold) {
-            responseBuilder.up()
-                    .withData("usage", "Obecne zuzycie pamieci  %s%%".formatted(memoryUsage * 100));
-        } else {
-            responseBuilder.down().withData("usage", "Obecne zuzycie pamieci %s %%".formatted(memoryUsage * 100));
-        }
-        return responseBuilder.build();
-    }
-}
+//package pl.lodz.p.it.ssbd2023.ssbd03.health;
+//
+//import jakarta.enterprise.context.ApplicationScoped;
+//import jakarta.inject.Inject;
+//import org.eclipse.microprofile.config.inject.ConfigProperty;
+//import org.eclipse.microprofile.health.HealthCheck;
+//import org.eclipse.microprofile.health.HealthCheckResponse;
+//import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+//import org.eclipse.microprofile.health.Liveness;
+//
+//@Liveness
+//@ApplicationScoped
+//public class MemoryMonitor implements HealthCheck {
+//    @Inject
+//    @ConfigProperty(name = "memory.threshold", defaultValue = "0.9")
+//    double memoryThreshold;
+//
+//    @Override
+//    public HealthCheckResponse call() {
+//        HealthCheckResponseBuilder responseBuilder = HealthCheckResponse.named("Zuzycie pamieci dla SSBD03");
+//        long freeMemory = Runtime.getRuntime().freeMemory();
+//        long maxMemory = Runtime.getRuntime().maxMemory();
+//        double memoryUsage = (double) (maxMemory - freeMemory) / maxMemory;
+//        if (memoryUsage <= memoryThreshold) {
+//            responseBuilder.up()
+//                    .withData("usage", "Obecne zuzycie pamieci  %s%%".formatted(memoryUsage * 100));
+//        } else {
+//            responseBuilder.down().withData("usage", "Obecne zuzycie pamieci %s %%".formatted(memoryUsage * 100));
+//        }
+//        return responseBuilder.build();
+//    }
+//}
