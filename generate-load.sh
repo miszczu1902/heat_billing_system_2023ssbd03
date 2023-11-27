eccbcfe20e8a9c7cf8dfdd2c74e3b542759ef83c
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-11-27 12:32:55 +0100

    Koncowe prace

--- generate-load.sh
+++ generate-load.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
-USERNAME="mariasilva"
-PASSWORD="Password$123"
-LOGIN_URL="http://ssbd03-tua202303.apps.okd.cti.p.lodz.pl/api/accounts/login"
-
-TOKEN=$(curl -s -i -X POST -H "Content-Type: application/json" -d '{"username": "'$USERNAME'", "password": "'$PASSWORD'"}' $LOGIN_URL | grep -i "^Bearer:" | awk '{print $2}' | tr -d '\r\n')
-
 execute_ab() {
-      ab -n 100 -c 100 -H "Authorization: Bearer $TOKEN" http://ssbd03-tua202303.apps.okd.cti.p.lodz.pl/api/accounts/self
+    local token=$1
+    local url="https://ssbd03-tua202303.apps.okd.cti.p.lodz.pl/api/accounts/self"
+    if [ -z "$token" ]; then
+        echo "Nie podano tokenu."
+        exit 1
+    fi
+    while true; do
+        ab -n 100 -c 100 -H "Authorization: Bearer $token" "$url"
+        sleep 1
+    done
 }
-echo $TOKEN;
-if [ -n "$TOKEN" ]; then
-    execute_ab
-else
-    echo "Nie udało się uzyskać tokenu JWT. Sprawdź dane logowania lub adres logowania."
-fi
+TOKEN="$1"
+execute_ab "$TOKEN"
