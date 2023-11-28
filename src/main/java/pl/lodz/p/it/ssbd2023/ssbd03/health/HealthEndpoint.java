b2fb2ad555f57c51a4945d1a18ddffa23a2ed4df
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-11-28 09:11:00 +0100

    Prace nad poprawkami health check

--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/HealthEndpoint.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/HealthEndpoint.java
@@ -3,12 +3,15 @@ package pl.lodz.p.it.ssbd2023.ssbd03.health;
 import jakarta.annotation.security.PermitAll;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/health-check")
 @PermitAll
 public class HealthEndpoint {
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @PermitAll
     public Response checkHealthy() {
         return Response.ok().build();
--- src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/StartupMonitor.java
+++ src/main/java/pl/lodz/p/it/ssbd2023/ssbd03/health/StartupMonitor.java
@@ -1,10 +1,12 @@
 package pl.lodz.p.it.ssbd2023.ssbd03.health;
 
+import io.quarkus.runtime.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
-import org.eclipse.microprofile.health.Startup;
+import org.eclipse.microprofile.health.Liveness;
 
+@Liveness
 @Startup
 @ApplicationScoped
 public class StartupMonitor implements HealthCheck {
